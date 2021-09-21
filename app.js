54866d9868f572641f8df8c15c57dfc21b9c94ac
Raj kapoor <89578075+Rajkapoor51@users.noreply.github.com>
2021-09-21 18:23:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue=" ";
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText=e.target.innerText;
+        console.log("button text is",buttonText);
+        if(buttonText=='x'){
+            buttonText='*';
+            screenValue+=buttonText;
+            screen.value = screenValue;
+        }
+        else if(buttonText=='C'){
+            screenValue =' ';
+            screen.value = screenValue;
+        }
+        else if(buttonText=='Backspace'){
+            screenValue =screen.value;
+            screenValue = screenValue.slice(0,-1);
+            screen.value=screenValue;
+        }
+        else if(buttonText=='sins'){
+            screenValue=Math.sin(screen.value*(Math.PI/100));
+            screen.value=screenValue;
+
+            
+        }
+        else if(buttonText=='coss'){
+            screen.value=Math.cos(screen.value*(Math.PI/100));
+            screen.value=screenValue;
+        }
+        else if(buttonText=='='){
+            screen.value = eval(screenValue);
+        }
+        else{
+            screenValue+=buttonText;
+            screen.value = screenValue;
+
+        }
+    })
+}
